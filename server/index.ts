ccd1fa2bc7060a83884cce58e9e5d97573c79460
Batuhan Ozturk <fbatuhanr@gmail.com>
2024-03-22 13:50:08 +0300

    vite project

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 
 const app = express();
 const server = createServer(app);
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
-server.listen(3001, () => {
+server.listen(3000, () => {
   console.log('server running at http://localhost:3000');
 });
