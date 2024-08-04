cb3c0c6c4d53587633f0e7943a5b38a204ccab65
Batuhan Ozturk <fbatuhanr@gmail.com>
2024-08-05 01:07:03 +0300

    Update user.route.ts

--- server/src/features/user/user.route.ts
+++ server/src/features/user/user.route.ts
@@ -19,10 +19,8 @@ router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.post('/sign-up', userController.signup);
 
-router.get('/search', authenticateToken, userController.searchUsers);
-
 router.get('/find', userController.getByUsername);
-
+router.get('/search', authenticateToken, userController.searchUsers);
 router.get('/', authenticateToken, userController.getAllWithLimitation);
 router.get('/:id', authenticateToken, userController.get);
 
