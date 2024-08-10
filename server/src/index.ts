a662440037c46388043c6e3d757df66213f03c1f
Batuhan Ozturk <fbatuhanr@gmail.com>
2024-08-10 18:33:59 +0300

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,7 +26,7 @@ mongoose
 const app: Express = express();
 const port = process.env.PORT || 3001;
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
   credentials: true
 };
 
